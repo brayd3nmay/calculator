02c890d4a4a2cd3fb996060200737197952855f1
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-12-09 19:19:24 -0500

    Add operate function to call operands

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let numA = 0;
+let numB = 0;
+let operand = "undefined";
+
+function operate(a, b, operand) {
+    switch (operand) {
+        case "add": {
+            return add(a, b);
+        }
+        case "subtract": {
+            return subtract(a, b);
+        }
+        case "multiply": {
+            return multiply(a, b);
+        }
+        case "divide": {
+            return divide(a, b);
+        }
+    }
+}
